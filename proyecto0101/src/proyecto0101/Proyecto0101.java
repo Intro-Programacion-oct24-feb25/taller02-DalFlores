49dec5b9a0923364a450ce4c791955a2146ea069
Dalton Flores <daltonflores3456@gmail.com>
2024-10-17 11:08:48 -0500

    primer commit

---
+++ proyecto0101/src/proyecto0101/Proyecto0101.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto0101;
+
+/**
+ *
+ * @author utpl
+ */
+public class Proyecto0101 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
